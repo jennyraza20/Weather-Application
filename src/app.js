4fd3b233f65f24771b96255dfd3dbe6cc9923b33
Jenny Raza <erika.jenny@gmail.com>
2021-03-01 20:28:16 -0700

    Updated date to fetch real time data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formattedDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response);
   let temperatureElement = document.querySelector("#local-temp");
@@ -6,6 +30,7 @@ function displayTemperature(response) {
   let feelsLikeElement = document.querySelector("#feels");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -13,9 +38,11 @@ function displayTemperature(response) {
   feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formattedDate(response.data.dt * 1000);
 }
 
 let apiKey = "73277c7b18b5c07a81fafdff51fe6d88";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kingston&appid=${apiKey}&units=metric`;
+let city = `Kingston`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
